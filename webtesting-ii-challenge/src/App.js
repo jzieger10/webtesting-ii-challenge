d8f35a14ba9b1dc28e51a12378ba9986ec3c68f9
Lambda_School_Loaner_72 <jzieger10@gmail.com>
2019-05-15 19:25:02 -0400

    Added bases and runs

--- webtesting-ii-challenge/src/App.js
+++ webtesting-ii-challenge/src/App.js
@@ -8,6 +8,10 @@ class App extends React.Component {
 		fouls: 0,
 		outs: 0,
 		inning: 0.5,
+		bases: 0,
+		totalBases: 0,
+		teamARuns: 0,
+		teamBRuns: 0,
 	};
 
 	newBatter = () => {
@@ -40,22 +44,56 @@ class App extends React.Component {
 	};
 
 	hit = () => {
+		let bases = Math.floor(Math.random() * Math.floor(5));
+		this.setState({ totalBases: this.state.totalBases + bases });
+		if (this.state.totalBases % 4 === 0) {
+			// this logic is not correct
+			this.state.inning % 1 === 0
+				? this.setState({
+						bases: 0,
+						teamBRuns: this.state.teamBRuns + 1,
+				  })
+				: this.setState({
+						bases: 0,
+						teamARuns: this.state.teamARuns + 1,
+				  });
+		} else {
+			this.setState({
+				totalBases: this.state.totalBases + bases,
+			});
+		}
+		this.setState({ bases: bases });
 		this.newBatter();
 	};
 
 	out = () => {
 		this.state.outs < 2
 			? this.setState({ outs: this.state.outs + 1 })
-			: this.setState({ inning: this.state.inning + 0.5 });
+			: this.newInning();
 	};
+
+	newInning = () => {
+		this.setState({
+			outs: 0,
+			inning: this.state.inning + 0.5,
+			bases: 0,
+			totalBases: 0,
+		});
+	};
+
+	// addARun = () => {
+	//   this.state.totalBases > 4 ? this.setState({ })
+	// }
 
 	render() {
 		return (
 			<div className='App'>
 				<header className='App-header'>
 					<div className='counts'>
+						<h1>Team A Score: {this.state.teamARuns}</h1>
+						<h1>Team B Score: {this.state.teamBRuns}</h1>
 						<h1>
-							Inning:{" "}
+							Inning:
 							{this.state.inning % 1 === 0
 								? `Bottom ${this.state.inning.toFixed(0)}`
 								: `Top ${this.state.inning.toFixed(0)}`}
@@ -63,7 +101,9 @@ class App extends React.Component {
 						<h2>Outs: {this.state.outs}</h2>
 						<h3>Strikes: {this.state.strikes}</h3>
 						<h3>Balls: {this.state.balls}</h3>
-						<h3>Fouls: {this.state.fouls}</h3>
+						<h4>Bases: {this.state.bases}</h4>
+						<h2>Total Bases: {this.state.totalBases}</h2>
+						{/* <h3>Fouls: {this.state.fouls}</h3> */}
 					</div>
 					<div>
 						<button onClick={this.strike}>Strike</button>
